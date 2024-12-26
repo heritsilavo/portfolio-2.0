43ccbf2a60ac1e62d590d3e8a3e592db33b86ad9
heritsilavo <heritsilavo4835@gmail.com>
2024-12-26 22:25:31 +0300

    stickAndScroll

---
+++ app/stickAndScroll/page.tsx
@@ -0,0 +1,8 @@
+export default function StickAndScroll() {
+  return (
+    <div className="w-full bg-blue-950 h-[100vh]">
+      <div className="bg-gray-500 bg-opacity-50 w-full h-[100vh] text-slate-300 text-5xl flex items-center justify-center">Space before</div>
+      <div className="bg-gray-500 bg-opacity-50 w-full h-[100vh] text-slate-300 text-5xl flex items-center justify-center">Space after</div>
+    </div>
+  );
+}

25e42d86ceb7c85602a9a2127dce8a6a877c7a2a
heritsilavo <heritsilavo4835@gmail.com>
2025-02-10 21:52:41 +0300

    10 fevr 2025

--- components/MainComponent/MainComponent.tsx
+++ components/MainComponent/MainComponent.tsx
@@ -1,24 +1,28 @@
 "use client";
 import React, { createContext, useContext, useRef } from "react";
-import "./MainComponent.css"
+import "./MainComponent.css";
+import ModalComponent from "../ModalComponent/ModalComponent";
 
 type MainComponentProps = {
   children: React.ReactNode;
 };
 
-export const MainRefContext = createContext<React.RefObject<HTMLDivElement> | null>(null);
+export const MainRefContext =
+  createContext<React.RefObject<HTMLDivElement> | null>(null);
 
 export default function MainComponent({ children }: MainComponentProps) {
   const mainRef = useRef<HTMLDivElement>(null);
 
   return (
     <MainRefContext.Provider value={mainRef}>
-      <main
-        ref={mainRef}
-        className="__scrollable__main__ w-[100vw] h-[100dvh] overflow-x-hidden overflow-y-auto absolute top-0 left-0"
-      >
-        {children}
-      </main>
+      <ModalComponent>
+        <main
+          ref={mainRef}
+          className="__scrollable__main__ w-[100vw] h-[100dvh] overflow-x-hidden overflow-y-auto absolute top-0 left-0"
+        >
+          {children}
+        </main>
+      </ModalComponent>
     </MainRefContext.Provider>
   );
 }
---
+++ components/ModalComponent/ModalComponent.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { createContext, useContext, useState } from "react";
+import "./ModalComponent.css"
+
+type ModalComponentProps = {
+  children: React.ReactNode;
+};
+
+type ModalContextType = {
+  open: boolean;
+  content: React.ReactNode;
+  header?: string;
+  btnsLabel?: {
+    cancel: string,
+    confirm: string
+  }
+};
+
+const ModalContext = createContext<{
+  modal: ModalContextType;
+  setModal: React.Dispatch<React.SetStateAction<ModalContextType>>;
+}>({ modal: { content: null, open: false }, setModal: () => {} });
+
+export default function ModalComponent({ children }: ModalComponentProps) {
+  const [modal, setModal] = useState<ModalContextType>({
+    content: null,
+    open: false,
+  });
+
+  const hideModal = ()=>{setModal(()=>({...modal, open:false}))};
+
+  return (
+    <ModalContext.Provider value={{ modal, setModal }}>
+      {
+        modal.open && <div onClick={hideModal}  className="modal-transparent-background">
+        <div className="modal-container">
+          <div className="modal-header">
+            {modal.header || "Modal header"}
+          </div>
+          <div className="modal-content"></div>
+          <div className="modal-footer">
+            <button onClick={hideModal} className="modal-btn modal-btn-cancel">{modal.btnsLabel?.cancel || "Annuler"}</button>
+            <button onClick={hideModal} className="modal-btn modal-btn-confirm">{modal.btnsLabel?.confirm || "Confirmer"}</button>
+          </div>
+        </div>
+      </div>
+      }
+
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export const useModal = () => {
+    return useContext(ModalContext);
+}
